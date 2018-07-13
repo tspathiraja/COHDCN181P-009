e49fab9079b5ee8960fa7586f1d597a7634f066a
tspathiraja <mptspathiraja@gmail.com>
2018-07-13 21:13:43 +0530

    homework_01

---
+++ homework01.py
@@ -0,0 +1,10 @@
+i=0
+s=0
+while True:
+    a=raw_input("Enter Value: ")
+    if a=='':                         #press 'enter key' loop end & print s
+        break
+    elif a.isdigit()==True:           #check value'a' is type int
+        i=int(a)
+        s=s+i
+print('Sum of the Your Values: %d' %s)
