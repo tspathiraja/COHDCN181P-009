dab53326ff129e35dbe3f085808d1e4c42301d3e
tspathiraja <mptspathiraja@gmail.com>
2018-07-14 11:34:36 +0530

    homework_01

--- homework01.py
+++ homework01.py
@@ -3,7 +3,7 @@ while True:
     a=raw_input("Enter Value: ")
     if a=='':                         #press 'enter key' loop end
         break
-    elif a.isdigit()==True:           #check value'a' is integer
+    elif a.isdigit()==True:           #check value 'a' is integer
         i=int(a)
         s=s+i
 print('Sum of the Your Values: %d' %s)
