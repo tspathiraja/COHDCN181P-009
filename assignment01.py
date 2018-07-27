976f5c37971d4d6edb0d71874ba4b1f2702f7cc4
tspathiraja <tspathiraja@github.com>
2018-07-27 20:23:56 +0530

    Final xxd code

--- assignment01.py
+++ assignment01.py
@@ -26,8 +26,7 @@ def xxd(file_path):
         print('{0}: {1:<39} {2}'.format(step,' '.join(he2),''.join(ch)))
         s=s+1
 
-if __name__=='__main__':
-    if not os.path.exists(sys.argv[1]):
-        print('your enter is wrong')
-        sys.exit(1)
-    xxd(sys.argv[1])
+if not os.path.exists(sys.argv[1]):
+    print('your enter is wrong')
+    sys.exit(1)
+xxd(sys.argv[1])
