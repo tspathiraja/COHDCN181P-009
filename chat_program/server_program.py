dbba788f8db67be0a5469c674864bbe64e8a6883
tspathiraja <tspathiraja@github.com>
2018-09-05 09:53:17 +0530

    chat_program

--- chat_program/server_program.py
+++ chat_program/server_program.py
@@ -6,22 +6,35 @@ def SE():
     while True:
         data = raw_input()
         c.send(data)
-    
-def RE():
-    while True:
-        DATA = c.recv(1024)
-        print (DATA)
-            
-host = sys.argv[1]
-port = int(sys.argv[2])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind((host, port))
-s.listen(1)
-c, addr = s.accept()
+try:
+    host = sys.argv[1]
+    port = int(sys.argv[2])
 
-T1 = Thread(target = SE).start()
-T2 = Thread(target = RE).start()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(1)
+    c, addr = s.accept()
 
+    T1 = Thread(target = SE)
+    T1.daemon = True
+    T1.start()
+
+except KeyboardInterrupt:
+        s.close()
+        sys.exit()
+
+while True:
+    try:
+        Data = c.recv(1024)
+        print (Data)
+
+    except KeyboardInterrupt:
+        s.close()
+        sys.exit()
+
+    else:
+        if not Data:
+            break
 
